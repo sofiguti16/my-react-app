120c90437a95ec7538c87a27d092e001c436bd66
Sofia Gutierrez <g.sofia@wustl.edu>
2024-11-13 21:17:49 -0600

    Store Values

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-function Square({ value }) {
+import { useState } from 'react';
+
+function Square() {
+
+  const [value, setValue] = useState(null);
+
   function handleClick() {
     console.log('clicked!');
   }
@@ -15,22 +20,22 @@ function Square({ value }) {
 
 export default function Board() {
   return (
-      <>
-        <div className="board-row">
-          <Square value="1" />
-          <Square value="2" />
-          <Square value="3" />
-        </div>
-        <div className="board-row">
-          <Square value="4" />
-          <Square value="5" />
-          <Square value="6" />
-        </div>
-        <div className="board-row">
-          <Square value="7" />
-          <Square value="8" />
-          <Square value="9" />
-        </div>
-      </>
-    );
-  }
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
